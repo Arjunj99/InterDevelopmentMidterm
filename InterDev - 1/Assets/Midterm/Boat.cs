43d53c47dda69cace14a0a485a203b30f67ff571
Arjunj99 <asj397@nyu.edu>
2019-10-09 21:13:15 -0400

    Boat AI and Class System

---
+++ InterDev - 1/Assets/Midterm/Boat.cs
@@ -0,0 +1,51 @@
+﻿public class Boat {
+    private string name;
+    private float currentSpeed;
+    private bool inMotion;
+    private float currentRotation;
+
+    
+    // private float d;
+    // private float moveSpeed;
+    // private float rotationSpeed;
+    // private float maxSpeed;
+    // private
+
+    public Boat() {
+        this.name = null;
+        currentSpeed = 0;
+        inMotion = false;
+        currentRotation = 0;
+    }
+    
+    public Boat(string name) {
+        this.name = name;
+        currentSpeed = 0;
+        inMotion = false;
+        currentRotation = 0;
+    }
+
+    public float GetCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public void SetCurrentSpeed(float speed) {
+        currentSpeed = speed;
+    }
+
+    public bool GetInMotion() {
+        return inMotion;
+    }
+
+    public void SetInMotion(bool motion) {
+        inMotion = motion;
+    }
+
+    public float GetCurrentRotation() {
+        return currentRotation;
+    }
+
+    public void SetCurrentRotation(float speed) {
+        currentRotation = speed;
+    }
+}
---
+++ InterDev - 1/Assets/Midterm/BoatAI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatAI : MonoBehaviour {
+    public Boat boat;
+    private float maxDist = 40f;
+    private float maxSpeed = 8f;
+
+    void Awake() {
+        boat = new Boat();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boat.SetInMotion(true);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Ray navRay = new Ray(this.transform.position, this.transform.forward);
+        Debug.DrawRay(this.transform.position, this.transform.forward, Color.cyan, 10f);
+
+        this.transform.Translate(new Vector3(0, 0, boat.GetCurrentSpeed() * Time.deltaTime));
+        boat.SetCurrentRotation(boat.GetCurrentSpeed() * -2f);
+
+        if (boat.GetCurrentSpeed() > 0f) {
+            boat.SetCurrentSpeed(boat.GetCurrentSpeed() - 0.1f);
+        }
+
+        if (Physics.Raycast(navRay, maxDist)) {
+            this.transform.Rotate(new Vector3(0, boat.GetCurrentRotation() * Time.deltaTime, 0));
+        } else {
+            if (boat.GetCurrentSpeed() < maxSpeed)
+                boat.SetCurrentSpeed(boat.GetCurrentSpeed() + 0.2f);
+        }
+    }
+}
